9756d4168144b939ace811306608895e8026582c
Ziyaad M <ocdev20@gmail.com>
2021-05-27 21:29:23 +0200

    Owner Home Landing page

---
+++ src/owner/home/HomeController.java
@@ -0,0 +1,15 @@
+package owner.home;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomeController {
+
+    @FXML
+    private Button updateDetailsButton, propertyButton, tenantButton, createStatementButton, exitButton, archivesButton;
+
+    @FXML
+    void initialize() {
+
+    }
+}
